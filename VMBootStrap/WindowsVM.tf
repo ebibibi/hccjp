0a9ebacb2d800802116f01f5699d5c722f9149ac
Masahiko Ebisuda <ebibibi@gmail.com>
2018-07-18 10:54:15 +0900

    add WinRM listner port rules.

--- VMBootStrap/WindowsVM.tf
+++ VMBootStrap/WindowsVM.tf
@@ -94,6 +94,30 @@ resource "azurerm_network_security_group" "networksecuritygroup" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "WinRM_HTTP"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "WinRM_HTTPS"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
