d13a9ac988e7160a0121ae1541c6243b7ef05203
Masahiko Ebisuda <ebibibi@gmail.com>
2018-07-23 10:29:28 +0900

    rm cache

--- Certificate/GenerateCSR.ps1
+++
@@ -1,9 +0,0 @@
-Install-Module Microsoft.AzureStack.ReadinessChecker -Force
-
-$subjectHash = [ordered]@{"OU"="AzureStack";"O"="HCCJP";"L"="Minato-ku";"ST"="Tokyo";"C"="JP"}
-$outputDirectory = "c:\AzureStackCSR"
-$IdentitySystem = "AAD"
-$regionName = 'iijhuawei'
-$externalFQDN = 'hccjp.org'
-
-Start-AzsReadinessChecker -RegionName $regionName -FQDN $externalFQDN -subject $subjectHash -RequestType SingleCSR -OutputRequestPath $OutputDirectory -IdentitySystem $IdentitySystem -IncludePaaS
