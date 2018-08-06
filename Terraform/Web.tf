dc0aa75c57ec7387098d97d59f904691fd6763dc
Masahiko Ebisuda <ebibibi@gmail.com>
2018-08-06 10:32:41 +0900

    add appserviceplan.

--- Terraform/Web.tf
+++ Terraform/Web.tf
@@ -2,3 +2,22 @@ resource "azurerm_resource_group" "webresourcegroup" {
         name = "HCCJPWeb"
         location = "${var.location}"
 }
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "HCCJPappserviceplan"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.webresourcegroup.name}"
+  kind                = "linux"
+  sku {
+      tier            = "Standard"
+      size            = "B1"
+      capacity        = "1"
+  }
+  
+  properties {
+    reserved = "true"
+    per_site_scaling = "false"
+  }
+
+
+}
