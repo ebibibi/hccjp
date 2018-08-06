02a2b32cade59ea370e14c49d8554cb2e1b23266
Masahiko Ebisuda <ebibibi@gmail.com>
2018-08-06 10:38:01 +0900

    apply auto format.

--- Terraform/Web.tf
+++ Terraform/Web.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "webresourcegroup" {
-        name = "HCCJPWeb"
-        location = "${var.location}"
+  name     = "HCCJPWeb"
+  location = "${var.location}"
 }
 
 resource "azurerm_app_service_plan" "appserviceplan" {
@@ -8,16 +8,15 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.webresourcegroup.name}"
   kind                = "linux"
+
   sku {
-      tier            = "Standard"
-      size            = "B1"
-      capacity        = "1"
+    tier     = "Standard"
+    size     = "B1"
+    capacity = "1"
   }
-  
+
   properties {
-    reserved = "true"
+    reserved         = "true"
     per_site_scaling = "false"
   }
-
-
 }
