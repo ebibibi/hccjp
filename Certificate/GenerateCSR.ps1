7aa0ddb2359447013132582d808ffc4d76b19653
Masahiko Ebisuda <ebibibi@gmail.com>
2019-01-07 13:56:00 +0900

    add Update-Module.

--- Certificate/GenerateCSR.ps1
+++ Certificate/GenerateCSR.ps1
@@ -1,9 +1,16 @@
 Install-Module Microsoft.AzureStack.ReadinessChecker -Force
+Import-Module Microsoft.AzureStack.ReadinessChecker
+Update-Module Microsoft.AzureStack.ReadinessChecker
 
 $subjectHash = [ordered]@{"OU"="AzureStack";"O"="HCCJP";"L"="Minato-ku";"ST"="Tokyo";"C"="JP"}
-$outputDirectory = "c:\AzureStackCSR"
+$outputDirectory = "c:\HCCJP\AzureStackCSR"
 $IdentitySystem = "AAD"
 $regionName = 'iijhuawei'
 $externalFQDN = 'hccjp.org'
+
+if((Test-Path $outputDirectory) -eq $false)
+{
+    New-Item -Path $outputDirectory -ItemType Directory -Force
+}
 
 Start-AzsReadinessChecker -RegionName $regionName -FQDN $externalFQDN -subject $subjectHash -RequestType SingleCSR -OutputRequestPath $OutputDirectory -IdentitySystem $IdentitySystem -IncludePaaS
